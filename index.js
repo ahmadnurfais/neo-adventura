75a257414e87aa8739fa29406735e4807e427ef7
Ahmad Nur Fais <adhsteiz@gmail.com>
2023-12-09 22:01:22 +0700

    Update index.js
    Clean up the index.js to prepare the main major update

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-/**
- * @format
- */
 
-import {AppRegistry} from 'react-native';
-import App from './chat_page/screens/HomeNavigator';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
